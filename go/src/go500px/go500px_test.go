b1af4ae5efc6c404e0807c50297c189595442e13
Jag Saund <jsaund@twitter.com>
2015-12-15 22:09:05 -0800

    Fix Test

--- go/src/go500px/go500px_test.go
+++ go/src/go500px/go500px_test.go
@@ -3,9 +3,12 @@ package go500px
 import "testing"
 
 func TestGetPhotos(t *testing.T) {
-	s := GetPhotos("8C6ImXPi4dKEnOWC3YwPnKQO1QIYbqaystDCsijC")
-	if s == "" {
+	photos, err := GetPhotos("8C6ImXPi4dKEnOWC3YwPnKQO1QIYbqaystDCsijC")
+	if err != nil {
+		t.Fatalf("Failed to execute request. Reason: %v", err)
+	}
+
+	if photos == nil {
 		t.Fatalf("Failed to get response")
 	}
-	t.Logf("Response: %s\n", s)
 }
