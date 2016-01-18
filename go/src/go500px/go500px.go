6c58ce3e13fc4630e36bdb689a13a8595b33fd83
Jag Saund <jsaund@twitter.com>
2016-01-18 15:29:03 -0800

    Update Go API
    - add login and session APIs

--- go/src/go500px/go500px.go
+++ go/src/go500px/go500px.go
@@ -1,8 +1,29 @@
 package go500px
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/dghubble/oauth1"
+)
+
+const (
+	consumerKey      = "8C6ImXPi4dKEnOWC3YwPnKQO1QIYbqaystDCsijC"
+	consumerSecret   = "FJMl9qrMv2b96B103TYZbxkJpH7DsHkzWinAfUIg"
+	fiveHundredPxAPI = "https://api.500px.com/v1"
+)
 
 var client *http.Client
+var fiveHundredPxConfig = oauth1.Config{
+	ConsumerKey:    consumerKey,
+	ConsumerSecret: consumerSecret,
+	CallbackURL:    "",
+	Endpoint: oauth1.Endpoint{
+		RequestTokenURL: fiveHundredPxAPI + "/oauth/request_token",
+		AccessTokenURL:  fiveHundredPxAPI + "/oauth/access_token",
+		AuthorizeURL:    fiveHundredPxAPI + "/oauth/authorize",
+	},
+}
 
 func init() {
 	client = &http.Client{}
@@ -10,6 +31,19 @@ func init() {
 
 func getClient() *http.Client {
 	return client
+}
+
+func Login(username, password string, callback LoginCallback) {
+	login(username, password, fiveHundredPxConfig, callback)
+}
+
+func Session(token, secret string) error {
+	if token == "" || secret == "" {
+		return fmt.Errorf("Invalid token or secret")
+	}
+	clientToken := oauth1.NewToken(token, secret)
+	client = fiveHundredPxConfig.Client(oauth1.NoContext, clientToken)
+	return nil
 }
 
 func GetPhotos(builder GetPhotosRequestBuilder) (*Photos, error) {
