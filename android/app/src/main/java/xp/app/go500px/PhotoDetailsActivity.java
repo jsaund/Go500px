e2b05ebfe4c7840c04ae0b9a0fca380bb81e8fa5
Jag Saund <jsaund@twitter.com>
2016-01-24 14:28:26 -0800

    Add Comments Button to Photo Details

--- android/app/src/main/java/xp/app/go500px/PhotoDetailsActivity.java
+++ android/app/src/main/java/xp/app/go500px/PhotoDetailsActivity.java
@@ -2,9 +2,14 @@ package xp.app.go500px;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import go.go500px.Go500px;
 
@@ -17,12 +22,10 @@ public class PhotoDetailsActivity extends AppCompatActivity {
 
     @Override
     public void OnError(String s) {
-
     }
 
     @Override
     public void OnStart() {
-
     }
 
     @Override
@@ -53,11 +56,47 @@ public class PhotoDetailsActivity extends AppCompatActivity {
             .into(mPhoto);
         }
       });
+    }
+  }
 
+  private class PostCommentListener extends Go500px.PostCommentCallback.Stub {
+
+    @Override
+    public void OnError(final String s) {
+      runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+          mAddComment.setEnabled(true);
+          Toast.makeText(PhotoDetailsActivity.this, "Failed to post comment. Reason: " + s, Toast.LENGTH_LONG).show();
+        }
+      });
+    }
+
+    @Override
+    public void OnStart() {
+      runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+          mAddComment.setEnabled(false);
+        }
+      });
+    }
+
+    @Override
+    public void OnSuccess(final Go500px.PostCommentResponse postCommentResponse) {
+      runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+          mAddComment.setEnabled(true);
+          Log.d(TAG, "Status: " + postCommentResponse.GetStatus() + "\nMessage: " + postCommentResponse.GetMessage() + "\nError: " + postCommentResponse.GetError());
+          Toast.makeText(PhotoDetailsActivity.this, "Posted comment!", Toast.LENGTH_LONG).show();
+        }
+      });
     }
   }
 
   private final GetPhotoListener mGetPhotoListener = new GetPhotoListener();
+  private final PostCommentListener mPostCommentListener = new PostCommentListener();
 
   private ImageView mPhoto;
   private TextView mTitle;
@@ -66,11 +105,15 @@ public class PhotoDetailsActivity extends AppCompatActivity {
   private TextView mDescription;
   private TextView[] mUserComments;
   private TextView[] mComments;
+  private EditText mComment;
+  private Button mAddComment;
 
 
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_photo_details);
+
+    final String photoID = getIntent().getStringExtra(EXTRA_PHOTO_ID);
 
     mPhoto = (ImageView) findViewById(R.id.photo);
     mTitle = (TextView) findViewById(R.id.photo_title);
@@ -86,7 +129,23 @@ public class PhotoDetailsActivity extends AppCompatActivity {
     mComments[0]= (TextView) findViewById(R.id.photo_comments_1);
     mComments[1]= (TextView) findViewById(R.id.photo_comments_2);
 
-    final String photoID = getIntent().getStringExtra(EXTRA_PHOTO_ID);
+    mComment = (EditText) findViewById(R.id.comment);
+    mAddComment = (Button) findViewById(R.id.add_comment);
+    mAddComment.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        final String comment = mComment.getText().toString();
+        if (TextUtils.isEmpty(comment)) {
+          return;
+        }
+        final Go500px.PostCommentRequestBuilder postCommentBuilder = Go500px.NewPostCommentRequestBuilder(Config.BASE_URL);
+        postCommentBuilder
+          .Body(comment)
+          .PhotoID(photoID);
+        Go500px.PostCommentAsync(postCommentBuilder, mPostCommentListener);
+      }
+    });
+
     final Go500px.GetPhotoRequestBuilder getPhotoBuilder = Go500px.NewGetPhotoRequestBuilder(Config.BASE_URL);
     final byte comments = 1;
     getPhotoBuilder
